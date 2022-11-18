305a12ef511034fafd14ad6d8eea277d41603c8b
Chinmay Krishna <chinmaykrishna2505@gmail.com>
2022-11-18 18:01:35 +0530

    One of the first programs (as taught in school, CMS Gomti Nagar-1 Branch, Grade IX) of Nested `for` loop in Java - printing tables of numbers from `x` to `y` in separate columns.

---
+++ programs/src/pro_Tables.java
@@ -0,0 +1,28 @@
+/**
+ * This programs inputs two numbers - x and y, and prints their tables in separate colums
+ *
+ * All thanks to the Jugaad Technology (#JT, God Damn Harkirat Sir is hyperawesome!) of @abhisheksaxena11jul (GitHub)
+ * */
+import java.util.Scanner;
+
+public class pro_Tables {
+    public static void main(String[] args) {
+        Scanner tableau = new Scanner(System.in);
+
+        System.out.print("Enter the smaller number: ");
+        int x = tableau.nextInt();
+
+        System.out.print("Enter the bigger number");
+        int y = tableau.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = x; j <= y; j++) {
+                System.out.print(j + " * " + i + " = " + i*j + "\t");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
