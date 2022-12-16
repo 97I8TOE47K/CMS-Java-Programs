fa6587ac8aa5d60e6a6f2fa28ce67d5afbc7eacc
Chinmay Krishna <chinmaykrishna2505@gmail.com>
2022-12-16 20:30:37 +0530

    Ultra pro max program

--- programs/src/Challenge_Problems/DigitRepeated.java
+++ programs/src/Challenge_Problems/DigitRepeated.java
@@ -5,66 +5,26 @@ public class DigitRepeated {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Enter the number: ");
-        int N = inp.nextInt();
+        System.out.print("Enter a number: ");
+        int num = inp.nextInt();
 
         System.out.print("Enter the digit: ");
         int D = inp.nextInt();
 
         inp.close();
 
-        int digits = No_Of_Digits(N, D);
+        char[] numArr = (num + "").toCharArray();
+        char D_char = (char)(D + '0');
 
-        if (digits >= 2) {
-            System.out.println("Digit " + D + " is repeated in " + N);
-        }
-
-        else {
-            System.out.println("Digit " + D + " is *not* repeated in " + N);
-        }
-
-
-    }
-
-
-    /**
-     * @param Num The number
-     * @param Digit The digit whose occurrence is to be found
-     * @return No. of occurrences of the given digit in the number
-     */
-    public static int No_Of_Digits(int Num, int Digit) {
-
-        int digits = Power_of_Ten(Num);
-        int counter = 0;
-
-        for (int check = 1; check <= digits; check++) {
-            if (Num % 10 == Digit) {
-                counter++;
-            }
-
-            Num /= 10;
-        }
-
-        return counter;
-
-
-    }
-
-    /**
-     * @param num The number whose no. of digits is to be found
-     * @return No of digits in the given number
-     */
-    private static int Power_of_Ten(int num) {
-        int tp = 0;
-
-        for (int i = 1; ; i++) {
-            if (num % (int)(Math.pow(10,i)) == num) {
-                tp = i;
-                break;
+        outer:
+        for (int i = 0; i <= numArr.length - 1; i++) {
+            for (int j = 0; j <= numArr.length - 1; j++) {
+                if (i != j && (numArr[i] == numArr[j] && numArr[j] == D_char)) {
+                    System.out.println("The digit is repeated");
+                    break outer;
+                }
             }
         }
-
-        return tp;
     }
 }
 
