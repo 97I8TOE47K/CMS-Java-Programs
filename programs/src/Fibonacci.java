da06286716ffa5b22e33b6c0f38f74b349674abe
Chinmay Krishna <chinmaykrishna2505@gmail.com>
2023-01-31 19:14:55 +0530

    `Fibonacci.java` added

---
+++ programs/src/Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Fibo(7); // Replace 7 with your number of choice
+
+    }
+        private static void Fibo(int NE) {
+            int T1 = 0, T2 = 1;
+
+            System.out.println(T1);
+
+            for (int i = 1; i <= NE-1; i++) {
+                System.out.println(T2);
+                int temp = T2;
+                T2 += T1;
+                T1 = temp;
+
+        }
+
+    }
+}
+
